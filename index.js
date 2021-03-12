1c11a878fbac77b80a2ea029b407d58a6fcf0d2a
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-03-12 22:56:50 +0200

    Дз№11 решение№2

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 // Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд.
 // Значением исполнившегося промиса должно быть то кол - во миллисекунд которое передали во время вызова функции delay.
 
-// const delay = (ms) => {
-//     setTimeout(() => {
-//         const promis = new Promise(res,ms) => {
-//     promis = ms
-//       }
-//   }, 1000);
-// };
+const delay = (ms) => {
+  const promis = new Promise((resolved) => {
+    setTimeout(() => {
+      resolved(ms);
+    }, ms);
+  });
+  return promis;
+};
 
 const logger = (time) => console.log(`Resolved after ${time}ms`);
 
