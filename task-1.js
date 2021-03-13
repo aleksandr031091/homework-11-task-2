165cca34787ce63c13f0cd3f3dc31507feafadc1
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-03-13 21:48:42 +0200

    Дз№11 задача№2 решение№1,2,3

--- task-1.js
+++ task-1.js
@@ -4,12 +4,11 @@
 // Значением исполнившегося промиса должно быть то кол - во миллисекунд которое передали во время вызова функции delay.
 
 const delay = (ms) => {
-  const promis = new Promise((resolved) => {
+  return new Promise((resolved) => {
     setTimeout(() => {
       resolved(ms);
     }, ms);
   });
-  return promis;
 };
 
 const logger = (time) => console.log(`Resolved after ${time}ms`);
